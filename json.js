a3b8a800e77f2ab2e45c38329e45121c9fde335a
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-26 20:02:40 +0600

    working

---
+++ json.js
@@ -0,0 +1,48 @@
+const user = {
+    id: 1,
+    name: 'Ghonchu',
+    job: 'Writer'
+}
+
+console.log(user);
+
+// JavaScript Object Notation (JSON)
+
+const stringified = JSON.stringify(user);
+
+console.log(stringified);
+
+
+const shop = {
+    owner: 'Yellow',
+
+    address: {
+        street: 'Road no: 04',
+        location: 'Mirpur 2, Mirpur'
+    },
+
+    products : ['Shirt', 'Pant', 'Shoes', 'Belt', 'Others'],
+
+    revenue : 50000,
+
+    isOpen : true,
+
+    isNew : false
+}
+
+
+console.log(shop);
+
+console.log(typeof shop);
+
+
+const shopJson = JSON.stringify(shop);
+
+console.log(shopJson);
+
+console.log(typeof shopJson);
+
+
+const shopObj = JSON.parse(shopJson);
+
+console.log(shopObj);
