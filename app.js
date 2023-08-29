2961758a825cc913f1f3bb05dfb259b7c29f9540
antorahad <antorahad6@gmail.com>
2023-08-29 14:18:35 +0600

    api

--- app.js
+++ app.js
@@ -17,3 +17,29 @@ function loadData3(){
 function displayUsers(data){
     console.log(data);
 }
+
+
+
+const dreamGirl = [
+    {
+     sokina: {
+      name: "bbu",
+      height: "5.4",
+      family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+      age: undefined,
+      contactInfo: [
+       {
+        facebook: {
+         link: "https://www.facebook.com/",
+         followers: "12545",
+         status: "single",
+         friendsList: [{ name: "rofik" }, undefined],
+        },
+       },
+       { instagram: "https://www.instagram.com/" },
+      ],
+     },
+    },
+   ];
+
+   
